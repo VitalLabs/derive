6a87df32d590b2d6d807c14883e9201e91358790
Ian Eslick <eslick@alum.mit.edu>
2014-07-15 17:30:34 -0700

    Port to use React

--- src/derive/main.cljs
+++ src/derive/main.cljs
@@ -1,30 +1,42 @@
 (ns derive.main
   (:require [figwheel.client :as fw :include-macros true]
-            [reagent.core :as reagent]
+            [om.core :as om :include-macros true]
+            [sablono.core :as html :refer-macros [html]]
             [derive.repl :as repl]
             [derive.tools :as tools]
             [derive.debug-level :as debug-level]))
 
 (enable-console-print!)
 
-(defonce hello-world-text (reagent/atom "Hello world!"))
+(def root-div (.getElementById js/document "app"))
+(defonce state (atom {:text "Hello world, how are ya!" :count 0}))
 
-(def body (.-body js/document))
+(defn root-component
+  [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html
+       [:div
+        [:h2 "Om Application"]
+        [:p (:text app) " " (:count app)]
+        [:button {:on-click #(om/transact! app :count inc)} "Increment"]]))))
 
-(defn hello-world-component []
-  [:div#wrapper
-   [:div#hello @hello-world-text]])
+(defn stop-app []
+  (om/detach-root! root-div))
 
-(defn hello-world []
-  (reagent/render-component [hello-world-component] body))
+(defn start-app []
+  (om/root root-component state {:target root-div :shared {}}))
 
 (defn ^:export init [dev]
   (debug-level/set-level!)
   (tools/log "(init) dev:" dev)
   (repl/connect)
-  (hello-world))
+  (start-app))
 
 (fw/watch-and-reload
  :jsload-callback (fn []
-                    ;; (stop-and-start-my app)
-                    ))
+                    (stop-app)
+                    (start-app)))
+
+
