d0883db1ad8d44d9dfb78ce4d211d2c887010435
Ian Eslick <eslick@alum.mit.edu>
2014-07-21 23:04:57 -0700

    Initial end-to-end demo of Derive integrated with Om
    Load up Figwheel and at the browser REPL type this:
      (insert! db (assoc (db 1) :text "It's alive!"))

--- src/derive/main.cljs
+++ src/derive/main.cljs
@@ -1,5 +1,5 @@
 (ns derive.main
-  (:require-macros [derive.deps :refer [defn-derived]])
+  (:require-macros [derive.deps :refer [defn-derived with-tracked-dependencies]])
   (:require [clojure.core.reducers :as r]
             [figwheel.client :as fw :include-macros true]
             [om.core :as om :include-macros true]
@@ -77,13 +77,13 @@
        (d/sort (comparator >))
        (reduce + 0)))
 
-(defn derive-text
+(defn-derived derive-text
   [db id]
 ;  (let [tracker nil #_(default-tracker)]
 ;    (fn [db id]
 ;      (binding [*tracker* nil #_(tracker db [id] *tracker*)]
   (println "Computing Text")
-  (str (:text (db id))  " [" (derive-count db id) "]"))
+  (str (:text (db id))  " [" (derive-count db (inc id)) "]"))
 
 (defn inc-mod [modulus]
   (fn [old]
@@ -95,11 +95,14 @@
     om/IRender
     (render [_]
       (let [db (om/get-shared owner :db)]
-        (html
-         [:div
-          [:h3 "Om Application"]
-          [:p (derive-text db (inc (:current app)))]
-          [:button {:on-click #(om/transact! app :current (inc-mod 4))} "Next"]])))))
+        (with-tracked-dependencies
+          [parent result deps (deps/empty-deps db)]
+          (html
+           [:div
+            [:h3 "Om Application"]
+            [:p (derive-text db (inc (:current app)))]
+            [:button {:on-click #(om/transact! app :current (inc-mod 4))} "Next"]])
+          (deps/subscribe! db #(om/refresh! owner)))))))
 
 ;;
 ;; Setup and Lifecycle Management
